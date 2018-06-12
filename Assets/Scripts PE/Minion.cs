70b0426b91d9604059510ad8f705bc1c55b95d12
ddiep003@ucr.edu <ddiep003@ucr.edu>
2018-06-11 19:03:48 -0700

    Minions Stats and On Hit Behaviors Implemented.

---
+++ Assets/Scripts PE/Minion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Minion : MonoBehaviour {
+
+    [SerializeField]
+    protected float CurrentHP;
+    [SerializeField]
+    protected float MaxHP;
+    public virtual void ApplyDamage(float damage = 0)
+    {
+
+    }
+}
