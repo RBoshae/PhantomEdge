df4c28991fdd3299711ff4a402a32abd6b252cb8
Rick <rwboshae@gmail.com>
2018-06-11 20:02:11 -0700

    Added Water to Map

---
+++ Assets/SetStartPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartPosition : MonoBehaviour {
+
+    public Transform StartPosition;
+	// Use this for initialization
+	void Start () {
+        transform.position = StartPosition.position;
+	}
+	
+}
