78e4bb4bbf38ce2b4be8066a2c9ffdaf3173f49b
Christian D Alvarado <32400508+calva038@users.noreply.github.com>
2018-06-13 14:10:11 -0700

    Added Textures and other assets
    Substances, Bridge Animation, and animated Skeleton character asset

---
+++ Assets/calva assets/CharacterBehaviourScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The role that the player chooses to be (warrior, mage, )
+    public enum Weapon
+{
+    sword = 0,
+}
+
+public enum PlayerStates
+{
+    idle = 0,
+    walking = 1,
+    attacking = 2,
+    dead = 3
+}
+
+public class Player : CharacterBase
+{
+
+    public Weapon currentRole;
+    [SerializeField]
+    public PlayerStates state = PlayerStates.idle;
+    public Character_Istics playerStats;
+    //Player's Health
+    public int maxHealth;
+    public int health
+    {
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _health = Mathf.Clamp(value, 0, maxHealth);
+            if (_health == 0)
+            {
+                //Destroy(gameObject);
+                Debug.Log("Dead");
+            }
+        }
+    }
+
+    private int _health = 0;
+
+    private void Start()
+    {
+        maxHealth = _health = playerStats.Health;
+    }
+   
+}
---
+++ Assets/calva assets/Character_Istics.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stats class defining the basic properties of an entity.
+/// </summary>
+[CreateAssetMenu(fileName = "Characteristics", menuName = "Scriptable Objects/Characteristics")]
+public class Character_Istics : ScriptableObject
+{
+
+    //General Characteristics
+    public int Health = 100;
+
+}
