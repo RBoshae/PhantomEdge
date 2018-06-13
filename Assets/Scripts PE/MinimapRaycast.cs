992bf1958b01d055d816df56fb975362e6468e38
Rick <rwboshae@gmail.com>
2018-06-12 21:15:19 -0700

    minimap script

---
+++ Assets/Scripts PE/MinimapRaycast.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapRaycast : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstick))
+        {
+            RaycastHit hit;
+            int layerMask = 1 << LayerMask.NameToLayer("Minimap");
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+            {
+
+            }
+        }
+	}
+}
